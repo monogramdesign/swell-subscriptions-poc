0534aca16b41a87ef6c25a26c2e0d31c873e25ba
Claudio F <claudio@monogram.io>
2022-09-26 18:48:52 -0400

    cleaned up _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,26 +1,19 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
 
-class MyDocument extends Document {
-	render() {
-		return (
-			<Html>
-				<Head lang="en">
-					<link rel="preconnect" href="https://fonts.googleapis.com" />
-					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-					<link
-						href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
-						rel="stylesheet"
-					/>
-					<link rel="icon" href="/favicon.ico" />
-				</Head>
+export default function Document() {
+	return (
+		<Html>
+			<Head lang="en">
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+				<link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		)
-	}
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	)
 }
-
-export default MyDocument

f33ecd5cd4c60ae9dd26b54ed54964483cf368a9
Claudio F <claudio@monogram.io>
2022-09-27 11:39:36 -0400

    updated footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -6,6 +6,27 @@ const Footer = () => {
 				<p className="text-sm text-[#6F6F6F] mt-4">
 					© 2022 Origin Coffee Co., a Family-Owned Company
 				</p>
+				<p className="text-sm text-gray-400">
+					Designed and developed by{' '}
+					<a
+						className="text-origin-citrus"
+						href="https://monogram.io/"
+						rel="noreferrer"
+						target="_blank"
+					>
+						monogram.io
+					</a>{' '}
+					for{' '}
+					<a
+						className="text-origin-citrus"
+						href="https://www.swell.is/"
+						rel="noreferrer"
+						target="_blank"
+					>
+						Swell
+					</a>
+					.
+				</p>
 			</div>
 		</footer>
 	)
--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -3,7 +3,13 @@ import React from 'react'
 const About = () => {
 	return (
 		<section className="section">
-			<div className="container">About US</div>
+			<div className="container">
+				<h1 className="text-lg">About Us</h1>
+				<p>
+					Monogram is a digital agency from Atlanta, specializing in headless ecommerce, web
+					development, and web apps.
+				</p>
+			</div>
 		</section>
 	)
 }
